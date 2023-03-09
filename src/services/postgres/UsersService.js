dc60693db9d9ab9c46637b0b282796606a5ad33c
RaidyMR <raidymustafid39@gmail.com>
2023-03-09 20:48:48 +0700

    authentications implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,8 +1,9 @@
-const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -20,10 +21,10 @@ class UsersService {
     };
 
     const result = await this._pool.query(query);
-
     if (!result.rows.length) {
       throw new InvariantError('User gagal ditambahkan');
     }
+
     return result.rows[0].id;
   }
 
@@ -34,7 +35,6 @@ class UsersService {
     };
 
     const result = await this._pool.query(query);
-
     if (result.rows.length > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
@@ -47,12 +47,41 @@ class UsersService {
     };
 
     const result = await this._pool.query(query);
-
     if (!result.rows.length) {
       throw new NotFoundError('User tidak ditemukan');
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
+  }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
   }
 }
 
